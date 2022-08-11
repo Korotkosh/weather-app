a00396a5c9412d5684d62819a21b7b8f15aa95f4
Korotkosh <108016188+Korotkosh@users.noreply.github.com>
2022-08-11 12:33:52 +0300

    Display the forcast data using the API and get new API key

--- src/index.js
+++ src/index.js
@@ -20,29 +20,50 @@ if (minute < 10) {
 let correctTime = document.querySelector(".time");
 correctTime.innerHTML = `${day}, ${hour}:${minute}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wen", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function forecastIcon() {
+  let farecastIconAPI = forecastDay.weather[0].icon;
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Tue", "Wen", "Thu", "Fri", "Sat"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      ` 
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        ` 
               <div class="col-2">
-                <div class="weater-forecast-day">${day}</div>
+                <div class="weater-forecast-day">${formatDay(
+                  forecastDay.dt
+                )}</div>
                 <img
-                  src="images/sunny.svg"
+                  src="http://openweathermap.org/img/wn/${iconElementAPI(
+                    forecastDay.weather[0].icon
+                  )}@2x.png"
                   alt="mostly cloudy"
                   class="weater-forecast-img"
                 />
                 <div class="weather-forecast-temperatures">
-                  <span class="weather-farecast-temperature-max">28°</span>
-                  <span class="weather-farecast-temperature-min">19°</span>
+                  <span class="weather-farecast-temperature-max">${Math.round(
+                    forecastDay.temp.max
+                  )}°</span>
+                  <span class="weather-farecast-temperature-min">${Math.round(
+                    forecastDay.temp.min
+                  )}°</span>
                 </div>
               </div>
       `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
